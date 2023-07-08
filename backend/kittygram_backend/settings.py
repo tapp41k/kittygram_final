b85ccf820443fbd214f4a46a0a4c438b8f5f8275
tapp41k <icepeak5@yandex.ru>
2023-07-09 00:21:59 +0300

    fixs

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
